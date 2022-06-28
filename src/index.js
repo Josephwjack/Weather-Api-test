ab3986b210bc8f46164cb74bcdc24f4aed12d0c9
Josephwjack <joseph.w.jack@gmail.com>
2022-06-28 10:09:46 -0700

    add promise with conditions on page loading or not
    Co-authored-by: Derrak Richard <derrak@gmail.com>

--- src/index.js
+++ src/index.js
@@ -9,31 +9,33 @@ $(document).ready(function() {
     const city = $('#location').val();
     $('#location').val("");
 
-    let request = new XMLHttpRequest();
-    const urls = [
-      {url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial` }
-    
-    ];
+    let promiseWeMade = new Promise(function(ifTrue, ifFalse){
+      let request = new XMLHttpRequest();
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`     
 
-    request.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
+      request.onload = function () {
+        if (this.status === 200) {
+          ifTrue(request.response);
+        } else {
+          ifFalse(request.response);
+        }
       }
-    };
+      request.open("GET", url, true);
+      request.send();
+    });
 
-
-
-    request.open("GET", urls, true);
-    request.send();
-
-    
-    // ${(1.8 * (response.main.temp - 273) + 32).toFixed(2)}
-    function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Fahrenheit is ${response.main.temp} degrees.`);
-      $('.showCloud').text(`The cloud condition is ${response.weather[0].description}.`);
-    }
-    
+    promiseWeMade.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Fahrenheit is ${body.main.temp} degrees.`);
+      $('.showCloud').text(`The cloud condition is ${body.weather[0].description}.`);
+      $('.showErrors').text("");
+      }, 
+      function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error}`);
+        $('.showHumidity').text("");
+        $('.showTemp').text("");
+        $('.showCloud').text("");
+    });
   });
 });
