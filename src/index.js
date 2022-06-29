5484ef0bc1ad5a9f81d0fedc05c3f40517d57a24
Josephwjack <joseph.w.jack@gmail.com>
2022-06-29 09:35:39 -0700

    add fetch api and refactor ui logic
    Co-authored-by: Derrak Richard <derrak@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+// import './css/styles.css';
 import WeatherService from './weather-service.js'
 
 function clearFields() {
@@ -13,20 +13,23 @@ function clearFields() {
   $('.showCloud').text("")
 }
 
+function getElements(response) {
+  if (response.main) {
+    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Fahrenheit is ${response.main.temp} degrees.`);
+    $('.showCloud').text(`The cloud condition is ${response.weather[0].description}.`);
+  } else {
+    $('.showErrors').text(`There was an error processing your request: ${response.message}`);
+  }
+}
+
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     clearFields();
-    let promiseWeMade = WeatherService.getWeather(city);
-    promiseWeMade.then(function(response) {
-      const body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Fahrenheit is ${body.main.temp} degrees.`);
-      $('.showCloud').text(`The cloud condition is ${body.weather[0].description}.`);
-      }, 
-      function(error) {
-        $('.showErrors').text(`There was an error processing your request: ${error}`);
-        
+    WeatherService.getWeather(city)
+      .then(function(response) {
+        getElements(response);
+      });
     });
   });
-});
--- src/weather-service.js
+++ src/weather-service.js
@@ -1,17 +1,14 @@
 export default class WeatherService {
   static getWeather(city) {
-    return new Promise(function(ifTrue, ifFalse) {
-      let request = new XMLHttpRequest();
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`;
-      request.onload = function () {
-        if (this.status === 200) {
-          ifTrue(request.response);
-        } else {
-          ifFalse(request.response);
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
+    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        } 
+        return response.json();
+        })
+      .catch(function(error) {
+        return error;
+    })
   }
 }
